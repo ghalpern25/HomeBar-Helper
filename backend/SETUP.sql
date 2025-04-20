fb29d78a154254f9af7b9c0d15ca0a50c704f605
Cole <cmccorkle416@gmail.com>
2025-04-20 03:06:18 +0000

    add setup.sql  with everything we need to test

--- backend/SETUP.sql
+++ backend/SETUP.sql
@@ -37,3 +37,76 @@ CREATE TABLE recipe_ingredients (
 CREATE TABLE user_inventory (
     ingredient_id INTEGER PRIMARY KEY REFERENCES ingredients(id) ON DELETE CASCADE
 );
+
+INSERT INTO units (name) VALUES
+('oz'),
+('mL'),
+('dash'),
+('cup'),
+('gal'),
+('unit'),
+('tsp'),
+('splash'),
+('barspoon'),
+('leaf');
+
+INSERT INTO ingredients (name) VALUES
+('Triple Sec'),
+('Maraschino Liqueur'),
+('Applejack'),
+('Kahlua'),
+('Tequila'),
+('Bourbon'),
+('St Germain'),
+('White Rum'),
+('Peach Schnapps'),
+('Baileys'),
+('Gin'),
+('Vodka'),
+('Chambord'),
+('Crown Apple'),
+('Grenadine'),
+('Club Soda'),
+('Pineapple Juice'),
+('Angosturra Bitters'),
+('Orange Bitters'),
+('Lemon Juice'),
+('Lime Juice'),
+('Sour Mix'),
+('Grape Juice'),
+('Cranberry Juice'),
+('Milk'),
+('Dry Vermouth'),
+('Simple Syrup'),
+('Egg Whites'),
+('Tonic Water'),
+('Ginger Beer'),
+('Orange Juice');
+
+-- Margarita
+INSERT INTO user_inventory (ingredient_id) VALUES
+(5),  -- Tequila
+(1),  -- Triple Sec
+(22); -- Lime Juice
+
+-- White Russian
+INSERT INTO user_inventory (ingredient_id) VALUES
+(12), -- Vodka
+(4),  -- Kahlua
+(25); -- Milk
+
+-- Whiskey Sour (missing egg whites)
+INSERT INTO user_inventory (ingredient_id) VALUES
+(6),  -- Bourbon
+(20), -- Lemon Juice
+(27); -- Simple Syrup
+
+INSERT INTO recipes (name, instructions) VALUES
+('Classic Margarita', 'Shake all ingredients with ice and strain into a salt-rimmed glass.'),
+('White Russian', 'Build in glass over ice and stir.'),
+('Whiskey Sour', 'Shake all ingredients with ice, strain into a rocks glass, and garnish with a cherry.');
+
+INSERT INTO recipes (name, instructions) VALUES
+('Classic Margarita', 'Shake all ingredients with ice and strain into a salt-rimmed glass.'),
+('White Russian', 'Build in glass over ice and stir.'),
+('Whiskey Sour', 'Shake all ingredients with ice, strain into a rocks glass, and garnish with a cherry.');
